13e1d04642556bd66cec45d3530ce55870447ec6
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-04 02:59:38 +0200

    js<>imported necessary packages and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,12 @@
+import SideNav from "./components/base/SideNav";
+import StatBar from "./components/base/StatBar";
+import { Route, Routes } from "react-router-dom";
 import "./styles/base.css";
+import Welcome from "./components/Welcome/Welcome";
+import AuthForm from "./components/AuthForm/AuthForm";
+import UserProvider from "./context/UserContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <></>
