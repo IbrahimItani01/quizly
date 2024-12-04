e0fe00bfea97cd8e7008698621e57a6151818260
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-04 21:14:18 +0200

    jsx<>replaced button tag with Button component

--- frontend/src/components/Quiz/Quiz.jsx
+++ frontend/src/components/Quiz/Quiz.jsx
@@ -77,56 +77,11 @@ const Quiz = () => {
             bgColor="#6A2C70"
             height="20px"
             labelAlignment="center"
-            labelColor="#fff"
-            baseBgColor="#ddd"
+          <Button
+            onClick={handleSubmitAnswer}
+            design={"outline"}
+            text={"Answer"}
           />
-
-          <h2>Question {currentQuestionIndex + 1}</h2>
-          <p>{quizQuestions[currentQuestionIndex].question}</p>
-
-          {quizQuestions[currentQuestionIndex].type === "multiple-choice" ? (
-            <ul>
-              {quizQuestions[currentQuestionIndex].options.map(
-                (option, index) => (
-                  <li key={index}>
-                    <label>
-                      <input
-                        type="radio"
-                        name={`question-${quizQuestions[currentQuestionIndex].id}`}
-                        value={option}
-                        onChange={(e) => setUserAnswer(e.target.value)}
-                      />
-                      {option}
-                    </label>
-                  </li>
-                )
-              )}
-            </ul>
-          ) : (
-            <input
-              type="text"
-              placeholder="Type your answer here"
-              name={`question-${quizQuestions[currentQuestionIndex].id}`}
-              value={userAnswer}
-              onChange={(e) => setUserAnswer(e.target.value)}
-            />
-          )}
-
-          {/* Submit Button */}
-          <button onClick={handleSubmitAnswer}>Submit Answer</button>
-
-          {/* Timer for each question */}
-          <div className="timer">
-            <CountdownCircleTimer
-              isPlaying
-              duration={15} // Duration for each question
-              colors={["#218380", "#F7B801", "#A30000"]}
-              colorsTime={[10, 5, 0]} // Change colors based on remaining time
-              onComplete={handleSubmitAnswer} // Automatically submit answer when time runs out
-            >
-              {({ remainingTime }) => <h4>Time Left: {remainingTime}s</h4>}
-            </CountdownCircleTimer>
-          </div>
         </div>
       ) : (
         <p>Loading quiz...</p>
