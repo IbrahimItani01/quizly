f8ad56a60f13e239f4ea275a7f9dae2d977445fc
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-04 21:04:14 +0200

    js<>commented out the auth logic for now

--- frontend/src/functions/authUser.js
+++ frontend/src/functions/authUser.js
@@ -4,27 +4,29 @@ import { loginUser, registerUser } from "../apis/users";
 export const authUser = (authType, authForm) => {
   if (authType === "login") {
     if (authForm.email && authForm.password) {
-      if (loginUser(authForm)) {
-        toast.success("Logged in 🎊");
-        localStorage.setItem("authenticated", "true");
-        return true;
-      } else {
-        toast.error("Something went wrong ⛔");
-        return false;
-      }
+      // if (loginUser(authForm)) {
+      //   toast.success("Logged in 🎊");
+      //   localStorage.setItem("authenticated", "true");
+      //   return true;
+      // } else {
+      //   toast.error("Something went wrong ⛔");
+      //   return false;
+      // }
+      return true;
     } else {
       toast.error("You missed an input 🤭");
     }
   } else {
     if (authForm.name && authForm.email && authForm.password) {
-      if (registerUser(authForm)) {
-        toast.success("Logged in 🎊");
-        localStorage.setItem("authenticated", "true");
-        return true;
-      } else {
-        toast.error("Something went wrong ⛔");
-        return false;
-      }
+      // if (registerUser(authForm)) {
+      //   toast.success("Logged in 🎊");
+      //   localStorage.setItem("authenticated", "true");
+      //   return true;
+      // } else {
+      //   toast.error("Something went wrong ⛔");
+      //   return false;
+      // }
+      return true;
     } else {
       toast.error("You missed an input 🤭");
     }
