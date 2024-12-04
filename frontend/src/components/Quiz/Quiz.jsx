e5ac8d700b5981224b6945649d8800c928a2b481
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-04 06:17:00 +0200

    js<>destructured required keys from different hooks

--- frontend/src/components/Quiz/Quiz.jsx
+++ frontend/src/components/Quiz/Quiz.jsx
@@ -4,3 +4,8 @@ import { userContext } from "../../context/UserContext";
 import { useParams, useNavigate } from "react-router-dom";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import ProgressBar from "@ramonak/react-progress-bar";
+// TODO: revamp
+const Quiz = () => {
+  const { questions, markQuizCompleted } = useContext(quizContext);
+  const { user, setUser } = useContext(userContext);
+  const { id } = useParams();
