c98bdc56c1a7236a8aa582aa483c35c75d97d4a7
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-04 06:14:06 +0200

    js<>fixed a logic issue to update score in stat bar in realtime

--- frontend/src/components/base/StatBar.jsx
+++ frontend/src/components/base/StatBar.jsx
@@ -1,15 +1,17 @@
 import React, { useContext } from "react";
 import { Star } from "lucide-react";
 import { userContext } from "../../context/UserContext";
+
 const StatBar = () => {
-  const { name, score } = useContext(userContext);
-  const token = localStorage.token
+  const { user } = useContext(userContext); // Access the user object from context
+  const token = localStorage.token;
+
   return (
     <div className="stat-bar">
-      <h1>✏️ Welcome, {name && localStorage.token ? name : "Human"}!</h1>
-      {score && token ? (
+      <h1>✏️ Welcome, {user && token ? user.name : "Human"}!</h1>
+      {user && user.score && token ? (
         <div className="stats-section">
-          <p>Score: {score}</p>
+          <p>Score: {user.score}</p>
           <Star fill="#009402" color="" />
         </div>
       ) : (
