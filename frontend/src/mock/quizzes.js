052dcf8278201a86fa09304ecfb40764721e6650
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-04 03:05:42 +0200

    js<>stored mock quizzes data

---
+++ frontend/src/mock/quizzes.js
@@ -0,0 +1,17 @@
+export const mockQuizzes = [
+  {
+    id: 1,
+    title: "General Knowledge",
+    description: "Test your general knowledge with fun questions!",
+  },
+  {
+    id: 2,
+    title: "Science Quiz",
+    description: "Explore the wonders of science!",
+  },
+  {
+    id: 3,
+    title: "History Trivia",
+    description: "How well do you know your history?",
+  },
+];
