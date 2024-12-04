902b01b17af36f2642415c8e28ac473e90536e2b
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-04 06:15:32 +0200

    js<>provided questions and mark quiz function in Quiz provider

--- frontend/src/context/QuizContext.jsx
+++ frontend/src/context/QuizContext.jsx
@@ -1,13 +1,18 @@
 import React, { createContext, useState } from "react";
-import { mockQuizzes } from "../mock/quizzes.js";
+import { mockQuestions, mockQuizzes } from "../mock/quizzes.js";
 
 export const quizContext = createContext();
 
 export const QuizProvider = ({ children }) => {
   const [quizzes, setQuizzes] = useState(mockQuizzes);
+  const [questions,setQuestions] = useState(mockQuestions);
+  const [completedQuizzes, setCompletedQuizzes] = useState([]);
 
+  const markQuizCompleted = (id) => {
+    setCompletedQuizzes((prev) => [...new Set([...prev, id])]);
+  };
   return (
-    <quizContext.Provider value={{ quizzes }}>
+    <quizContext.Provider value={{ quizzes, completedQuizzes, markQuizCompleted ,questions}}>
       {children}
     </quizContext.Provider>
   );
