b68676e412658fa583a1dc908555c58b1509a61d
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-04 03:28:28 +0200

    js<>integrated dynamic navigation from welcome page to auth forms

--- frontend/src/components/Welcome/Welcome.jsx
+++ frontend/src/components/Welcome/Welcome.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./style.css";
 import Button from "../Button/Button";
+import { useNavigate } from "react-router-dom";
 const Welcome = () => {
+  const navigate = useNavigate();
+  const handleNavigate = (e)=>{
+    e.target.textContent === "Login" ? navigate("/login"): navigate("/register")
+  }
   return (
     <div className="welcome-page">
       <div className="welcome-title">
@@ -9,8 +14,8 @@ const Welcome = () => {
         <h2>Quizly says: Sharpen your pencil ✏️</h2>
       </div>
       <div className="welcome-actions">
-        <Button text={"Login"} design={"outline"} />
-        <Button text={"get started"} design={"action"} />
+        <Button text={"Login"} design={"outline"} onClick={handleNavigate} />
+        <Button text={"get started"} design={"action"} onClick={handleNavigate} />
       </div>
     </div>
   );
