1919ff25074ac1753f27a2dc4d941e672de53dce
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-04 03:24:55 +0200

    js<>declared an external function to handle user authentication

---
+++ frontend/src/functions/authUser.js
@@ -0,0 +1,32 @@
+import { toast } from "react-toastify";
+import { loginUser, registerUser } from "../apis/users";
+
+export const authUser = (authType, authForm) => {
+  if (authType === "login") {
+    if (authForm.email && authForm.password) {
+      if (loginUser(authForm)) {
+        toast.success("Logged in 🎊");
+        localStorage.setItem("authenticated", "true");
+        return true;
+      } else {
+        toast.error("Something went wrong ⛔");
+        return false;
+      }
+    } else {
+      toast.error("You missed an input 🤭");
+    }
+  } else {
+    if (authForm.name && authForm.email && authForm.password) {
+      if (registerUser(authForm)) {
+        toast.success("Logged in 🎊");
+        localStorage.setItem("authenticated", "true");
+        return true;
+      } else {
+        toast.error("Something went wrong ⛔");
+        return false;
+      }
+    } else {
+      toast.error("You missed an input 🤭");
+    }
+  }
+};
