ec1c1b8fcb1e87948f8de608bf14d8c6e83ae5c9
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-04 03:47:31 +0200

    jsx<>added condition if user is authenticated or not

--- frontend/src/components/base/StatBar.jsx
+++ frontend/src/components/base/StatBar.jsx
@@ -3,10 +3,11 @@ import { Star } from "lucide-react";
 import { userContext } from "../../context/UserContext";
 const StatBar = () => {
   const { name, score } = useContext(userContext);
+  const token = localStorage.token
   return (
     <div className="stat-bar">
-      <h1>✏️ Welcome, {name ? name : "Human"}!</h1>
-      {score ? (
+      <h1>✏️ Welcome, {name && localStorage.token ? name : "Human"}!</h1>
+      {score && token ? (
         <div className="stats-section">
           <p>Score: {score}</p>
           <Star fill="#009402" color="" />
