6e824e62ba9f2b56da537b6762f53b5c19b72b75
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-04 06:14:57 +0200

    jsx<>rendered check icon if quiz is completed

--- frontend/src/components/QuizButton/QuizButton.jsx
+++ frontend/src/components/QuizButton/QuizButton.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./style.css";
-import { FileText } from "lucide-react";
-const QuizButton = ({ data }) => {
+import { CheckCircle, FileText } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+const QuizButton = ({ data, completed }) => {
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate(`/quiz/${data.id}`);
+  };
+useEffect(()=>{
+  console.log(completed);
+},[completed])
   return (
-    <div className="quiz-btn">
+    <div className={`quiz-btn ${completed ? "completed" : ""}`} onClick={handleNavigate}>
       <div className="btn-title">
-        <FileText />
-        <p>{data.title}</p>
+      {completed ? <CheckCircle color="#009402" /> : <FileText />}
+      <p>{data.title}</p>
       </div>
       <div className="separator"></div>
     </div>
