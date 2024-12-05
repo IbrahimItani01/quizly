52654792dbc9cdd963199b571a1ba0aa8dfbe0ae
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-06 00:46:43 +0200

    js<>instantiated a new Schema

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -3,3 +3,18 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true }, 
+  password: { type: String, required: true }, 
+  score: { type: Number, default: 0 }, 
+  completedQuizzes: [
+    {
+      quizId: { type: mongoose.Types.ObjectId, ref: "Quiz" }, 
+      score: { type: Number, required: true }, 
+      completedAt: { type: Date, default: Date.now }, 
+    },
+  ],
+}, {
+  timestamps: true, 
+});
