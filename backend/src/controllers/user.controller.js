d284fccd5d4d4eb937393f32d975c6e9f959c9c8
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-06 01:54:50 +0200

    js<>created a register controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import User from "../models/user.model.js";
 
 export const getAllUsers = async (req, res) => {
@@ -26,16 +28,32 @@ export const getUserById = async (req, res) => {
   }
 };
 
-export const createUser = async (req, res) => {
-  const { name, email, password, score } = req.body;
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
   try {
-    const newUser = new User({ name, email, password, score });
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, password: hashedPassword });
     await newUser.save();
-    res.status(201).json(newUser);
+
+    const token = jwt.sign(
+      { id: newUser._id, name: newUser.name, email: newUser.email },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+
+    res.status(201).json({ message: "User registered successfully", token });
   } catch (error) {
     res
       .status(500)
-      .json({ message: "Error creating user", error: error.message });
+      .json({ message: "Error registering user", error: error.message });
+  }
+};
+
   }
 };
 
