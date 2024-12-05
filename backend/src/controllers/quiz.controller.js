da9dac1f9441bbcfcd11a59ff8228c282e98c557
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-06 00:50:34 +0200

    js<>created a getAllQuizzes function in quiz controller

--- backend/src/controllers/quiz.controller.js
+++ backend/src/controllers/quiz.controller.js
@@ -1,2 +1,13 @@
 import Quiz from "../models/quiz.model.js";
 
+export const getAllQuizzes = async (req, res) => {
+  try {
+    const quizzes = await Quiz.find();
+    res.status(200).json(quizzes);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error fetching quizzes", error: error.message });
+  }
+};
+
