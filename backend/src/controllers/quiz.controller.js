2282a9dfcf45bed6ab678493007582f31e8a83e4
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-06 00:50:47 +0200

    js<>created a getQuizById function in quiz controller

--- backend/src/controllers/quiz.controller.js
+++ backend/src/controllers/quiz.controller.js
@@ -11,3 +11,17 @@ export const getAllQuizzes = async (req, res) => {
   }
 };
 
+export const getQuizById = async (req, res) => {
+  try {
+    const quiz = await Quiz.findById(req.params.id);
+    if (!quiz) {
+      return res.status(404).json({ message: "Quiz not found" });
+    }
+    res.status(200).json(quiz);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error fetching quiz", error: error.message });
+  }
+};
+
