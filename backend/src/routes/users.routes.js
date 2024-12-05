48da2a360b55bf701b20e51437f61caa535ba7ee
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-06 01:57:43 +0200

    js<>cleaned syntax

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -12,8 +12,7 @@ import authenticateJWT from "../middlewares/jwt.middleware.js";
 
 const router = express.Router();
 
-router.get("/", getAllUsers);
-
+router.get("/",getAllUsers);
 router.get("/:id", getUserById);
 router.post("/register", registerUser); 
 router.post("/login", loginUser); 
