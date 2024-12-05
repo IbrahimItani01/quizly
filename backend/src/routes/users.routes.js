b133fcb312ce3ff7c561ce936555dae46f341f5d
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-06 00:57:57 +0200

    js<>routed all controllers

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -9,3 +9,13 @@ import {
 
 const router = express.Router();
 
+router.get("/", getAllUsers);
+
+router.get("/:id", getUserById);
+
+router.post("/", createUser);
+
+router.put("/:id", updateUser);
+
+router.delete("/:id", deleteUser);
+
