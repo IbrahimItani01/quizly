99735f9b5a36c97d2bcedbf413e91d6e22b35311
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-06 00:48:17 +0200

    js<>instantiated a question schema

--- backend/src/models/quiz.model.js
+++ backend/src/models/quiz.model.js
@@ -3,3 +3,10 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 
+const questionSchema = new Schema({
+  id: { type: Number, required: true },
+  question: { type: String, required: true },
+  type: { type: String, enum: ["multiple-choice", "user-input"], required: true },
+  options: { type: [String], required: function () { return this.type === "multiple-choice"; } },
+  correctAnswer: { type: String, required: true },
+});
