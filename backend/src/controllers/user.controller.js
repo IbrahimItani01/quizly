83d211510545ff0919da4fabed4de8d1c397a383
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-06 01:55:15 +0200

    js<>created a loginUser api

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -54,6 +54,28 @@ export const registerUser = async (req, res) => {
   }
 };
 
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, name: user.name, email: user.email },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    res.status(500).json({ message: "Error logging in", error: error.message });
   }
 };
 
@@ -92,12 +114,10 @@ export const markQuizCompleted = async (req, res) => {
     await user.save();
     res.status(200).json(user);
   } catch (error) {
-    res
-      .status(500)
-      .json({
-        message: "Error marking quiz as completed",
-        error: error.message,
-      });
+    res.status(500).json({
+      message: "Error marking quiz as completed",
+      error: error.message,
+    });
   }
 };
 
