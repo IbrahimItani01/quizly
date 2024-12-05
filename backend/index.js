c4f64e9324dc0cbf019f6f595cedeb5e42c990b8
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-06 00:42:18 +0200

    js<>imported user and quiz routes

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./src/routes/users.routes.js";
+import quizRoutes from "./src/routes/quizzes.routes.js";
+
