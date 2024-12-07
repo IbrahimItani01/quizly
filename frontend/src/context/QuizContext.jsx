6a3d62f0ef9fea0d2c395289383609c9d98f0bd6
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-07 23:01:57 +0200

    js<>fetched quizzes data in quiz context

--- frontend/src/context/QuizContext.jsx
+++ frontend/src/context/QuizContext.jsx
@@ -1,16 +1,28 @@
-import React, { createContext, useState } from "react";
-import { mockQuestions, mockQuizzes } from "../mock/quizzes.js";
+import React, { createContext, useState, useEffect } from "react";
+import {getAllQuizzes } from "../apis/quizzes";
+import { completeQuiz } from "../apis/users";
 
 export const quizContext = createContext();
 
 export const QuizProvider = ({ children }) => {
-  const [quizzes, setQuizzes] = useState(mockQuizzes);
-  const [questions,setQuestions] = useState(mockQuestions);
+  const [quizzes, setQuizzes] = useState([]);
   const [completedQuizzes, setCompletedQuizzes] = useState([]);
 
-  const markQuizCompleted = (id) => {
-    setCompletedQuizzes((prev) => [...new Set([...prev, id])]);
-  };
+  useEffect(() => {
+    const fetchQuizzes = async () => {
+      const token = localStorage.token;
+      try {
+        const fetchedQuizzes = await getAllQuizzes(token);
+        console.log(fetchedQuizzes);
+        setQuizzes(fetchedQuizzes);
+      } catch (error) {
+        console.error("Failed to fetch quizzes:", error);
+      }
+    };
+
+    fetchQuizzes();
+  }, []);
+
   return (
     <quizContext.Provider value={{ quizzes, completedQuizzes, markQuizCompleted ,questions}}>
       {children}
