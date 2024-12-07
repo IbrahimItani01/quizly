539c01bf5185683ce3077ec099b7a24ef3fd1e41
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-07 22:53:33 +0200

    js<>handled fetching getAllquizzes api from frontend

--- frontend/src/apis/quizzes.js
+++ frontend/src/apis/quizzes.js
@@ -2,3 +2,14 @@ import axios from "axios";
 
 const baseUrl = "http://localhost:8800/api/quizzes"; // Update base URL as needed
 
+export const getAllQuizzes = async (token) => {
+  try {
+    const response = await axios.get(baseUrl, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching quizzes:", error);
+    throw error;
+  }
+};
