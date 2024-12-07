372c627171c39293b1fa93faa7d075739a548e40
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-07 22:58:22 +0200

    js<>uncommented the logic of fetching auth apis

--- frontend/src/functions/authUser.js
+++ frontend/src/functions/authUser.js
@@ -1,34 +1,34 @@
 import { toast } from "react-toastify";
 import { loginUser, registerUser } from "../apis/users";
 
-export const authUser = (authType, authForm) => {
-  if (authType === "login") {
-    if (authForm.email && authForm.password) {
-      // if (loginUser(authForm)) {
-      //   toast.success("Logged in 🎊");
-      //   localStorage.setItem("authenticated", "true");
-      //   return true;
-      // } else {
-      //   toast.error("Something went wrong ⛔");
-      //   return false;
-      // }
-      return true;
-    } else {
-      toast.error("You missed an input 🤭");
+export const authUser = async (authType, authForm) => {
+  try {
+    if (authType === "login") {
+      if (authForm.email && authForm.password) {
+        const response = await loginUser(authForm); // Call login API
+        toast.success("Logged in 🎊");
+        localStorage.setItem("authenticated", "true");
+        localStorage.setItem("token", response.token); // Store token
+        return response; // Return user data
+      } else {
+        toast.error("You missed an input 🤭");
+        return null;
+      }
+    } else if (authType === "register") {
+      if (authForm.name && authForm.email && authForm.password) {
+        const response = await registerUser(authForm); // Call register API
+        toast.success("Account created successfully 🎉");
+        localStorage.setItem("authenticated", "true");
+        localStorage.setItem("token", response.token); // Store token
+        return response; // Return user data
+      } else {
+        toast.error("You missed an input 🤭");
+        return null;
+      }
     }
-  } else {
-    if (authForm.name && authForm.email && authForm.password) {
-      // if (registerUser(authForm)) {
-      //   toast.success("Logged in 🎊");
-      //   localStorage.setItem("authenticated", "true");
-      //   return true;
-      // } else {
-      //   toast.error("Something went wrong ⛔");
-      //   return false;
-      // }
-      return true;
-    } else {
-      toast.error("You missed an input 🤭");
-    }
+  } catch (error) {
+    console.error("Authentication error:", error);
+    toast.error("Something went wrong ⛔");
+    return null;
   }
 };
