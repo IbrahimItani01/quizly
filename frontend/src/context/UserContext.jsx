0fa234217d8134a8f68849def7d1f37b61d84d04
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-07 23:03:06 +0200

    js<>fetched user data in user context

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,15 +1,25 @@
-import { createContext, useState } from "react";
-import { mockUsers } from "../mock/users";
+import { createContext, useState, useEffect } from "react";
+import { getUserData, updateUserScore} from "../apis/users";
 
 export const userContext = createContext();
 
-const UserProvider = ({children})=>{
-    const [user,setUser] = useState(mockUsers[0]);
-    return(
-        <userContext.Provider value={{user,setUser}}>
-            {children}
-        </userContext.Provider>
-    )
-}
+const UserProvider = ({ children, userId }) => {
+  const [user, setUser] = useState({});
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.token; // Get token from localStorage
+
+    if (token) {
+      getUserData(token)
+        .then((data) => {
+          setUser(data); // Set user data in state
+          setIsAuthenticated(true); // Mark user as authenticated
+        })
+        .catch((error) => {
+          console.error("Error fetching user data:", error);
+        });
+    }
+  }, []);
 
 export default UserProvider
