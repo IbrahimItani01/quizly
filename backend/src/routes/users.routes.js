fde25e211fc145f56d29342b48652d0071f38a91
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-07 22:52:12 +0200

    js<>renamed getUser data to /me

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -13,7 +13,7 @@ import authenticateJWT from "../middlewares/jwt.middleware.js";
 const router = express.Router();
 
 router.get("/",getAllUsers);
-router.get("/:id", getUserById);
+router.get("/me", authenticateJWT, getUserById);
 router.post("/register", registerUser); 
 router.post("/login", loginUser); 
 router.put("/:id/score",authenticateJWT, updateUserScore);
