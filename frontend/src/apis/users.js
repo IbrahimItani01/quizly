8acd5e64f2244f60e0f1b361adf836a7e737ea15
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-07 22:54:34 +0200

    js<>handled registering user api

--- frontend/src/apis/users.js
+++ frontend/src/apis/users.js
@@ -2,7 +2,20 @@ import axios from "axios";
 
 const baseUrl = "http://localhost:8800/api/users";
 
+export const registerUser = async (data) => {
+  try {
+    console.log("Registering user with data:", data);
+    const response = await axios.post(`${baseUrl}/register`, data, {
+      headers: { "Content-Type": "application/json" },
+    });
+    console.log("Response from server:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error(
+      "Error registering user:",
+      error.response?.data || error.message
+    );
+    throw error; // Rethrow the error for handling in the calling function
+  }
 };
-export const loginUser = (data) => {
-  // TODO: fetch login api
-};
+
