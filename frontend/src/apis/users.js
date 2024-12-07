25151cfb326724280c9e046ad21e9b870f86238e
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-07 22:54:15 +0200

    js<>stored base url of user apis route

--- frontend/src/apis/users.js
+++ frontend/src/apis/users.js
@@ -1,5 +1,7 @@
-export const registerUser = (data) => {
-  //  TODO: fetch register api
+import axios from "axios";
+
+const baseUrl = "http://localhost:8800/api/users";
+
 };
 export const loginUser = (data) => {
   // TODO: fetch login api
